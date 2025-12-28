1167871d23bf957be657d45ad9d25a4e21ad09fd
Colin Leroy-Mira <colin@colino.net>
2025-12-28 15:05:02 +0100

    Rename project, add proper licensing

--- SmartportSD-firmware/log.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LOG
-#define __LOG
-
-#define LOG(str)               do { Serial.println(str);                          } while (0)
-#define LOGN(str, num, base)   do { Serial.print(str); Serial.println(num, base); } while (0)
-#define DEBUG(str)             do { if (debug) LOG(str);                          } while (0)
-#define DEBUGN(str, num, base) do { if (debug) LOGN(str, num, base);              } while (0)
-
-#endif
---
+++ firmware/sp_vals.h
@@ -0,0 +1,24 @@
+#ifndef __SP_VALS
+#define __SP_VALS
+
+typedef enum _SP_State {
+  SP_BUS_DISABLED,
+  SP_BUS_RESET,
+  SP_BUS_ENABLED
+} SP_State;
+
+typedef enum _SP_Command {
+  SP_STATUS     = 0x80,
+  SP_READ       = 0x81,
+  SP_WRITE      = 0x82,
+  SP_FORMAT     = 0x83,
+  SP_INIT       = 0x85,
+
+  SP_EXT_STATUS = 0xC0,
+  SP_EXT_READ   = 0xC1,
+  SP_EXT_WRITE  = 0xC2,
+  SP_EXT_FORMAT = 0xC3,
+  SP_EXT_INIT   = 0xC5
+} SP_Command;
+
+#endif
